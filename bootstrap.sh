3a6ef96d68b5fb5f67cbe53db81d398cf2ecb8a1
Rupa Shankar <rupss@github.com>
2024-05-29 14:26:27 -0700

    Add dev ssh key setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,10 @@ git config --global core.editor "emacs"
 export GIT_EDITOR=emacs
 
 export GITHUB_TOKEN="$GH_GH_PAT"
+
+ssh-keygen -t ed25519 -C "monalisa@github.com" -q -N "" -f ~/.ssh/id_ed25519
+
+touch  ~/.ssh/config
+echo "Host github.com" >> ~/.ssh/config
+echo "  AddKeysToAgent yes" >> ~/.ssh/config
+echo "  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
